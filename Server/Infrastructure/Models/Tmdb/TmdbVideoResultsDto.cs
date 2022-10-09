a174be3b2f04492a6326052dd3bfc2082825dfaa
Kiko <kristiqnenchevv@gmail.com>
2022-10-09 14:09:11 +0300

    Video and Keyworkds dto's for TMDB

---
+++ Server/Infrastructure/Models/Tmdb/TmdbVideoResultsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Tmdb
+{
+    using System.Text.Json.Serialization;
+
+    public class TmdbVideoResultsDto
+    {
+        [JsonPropertyName("results")]
+        public List<TmdbVideoDto> Results { get; set; } = new();
+    }
+}
