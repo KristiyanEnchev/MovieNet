72966aea9c85527887a592b7da7dde84d85dce92
Kiko <kristiqnenchevv@gmail.com>
2022-10-10 12:38:22 +0300

    external api and cache options

---
+++ Server/Infrastructure/Models/CacheOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class CacheOptions
+    {
+        public string InstanceName { get; set; } = "MovieNet_";
+        public TimeSpan DefaultExpiry { get; set; } = TimeSpan.FromHours(1);
+        public bool EnableLogging { get; set; } = true;
+    }
+}
---
+++ Server/Infrastructure/Models/TmdbOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class TmdbOptions
+    {
+        public const string Section = "Tmdb";
+
+        public string BaseUrl { get; set; } = "https://api.themoviedb.org/3";
+        public string ApiKey { get; set; }
+        public string ImageBaseUrl { get; set; } = "https://image.tmdb.org/t/p/";
+        public int RequestsPerSecondLimit { get; set; } = 4;
+    }
+}
