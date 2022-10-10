8340e6b16997c3d49e51a8e29218f41280e01298
Kiko <kristiqnenchevv@gmail.com>
2022-10-10 14:55:37 +0300

    Add Movie Dtos

---
+++ Server/Infrastructure/Models/Movie/MovieDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Models.Movie
+{
+    public class MovieDetailsDto : MovieDto
+    {
+        public int Runtime { get; set; }
+        public string Status { get; set; }
+        public string Tagline { get; set; }
+        public string Homepage { get; set; }
+        public decimal Budget { get; set; }
+        public decimal Revenue { get; set; }
+        public string OriginalLanguage { get; set; }
+        public bool Adult { get; set; }
+
+        public List<GenreDto> Genres { get; set; } = new();
+        public List<CastDto> Cast { get; set; } = new();
+        public List<VideoDto>? Videos { get; set; } = new();
+    }
+}
---
+++ Server/Infrastructure/Models/Movie/MovieDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Models.Movie
+{
+    using Domain.Enums;
+
+    public class MovieDto
+    {
+        public string Id { get; set; }
+        public int TmdbId { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public string PosterPath { get; set; }
+        public string BackdropPath { get; set; }
+        public MediaType MediaType { get; set; }
+        public string ReleaseDate { get; set; }
+        public decimal? VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+        public decimal Popularity { get; set; }
+
+        public bool IsLiked { get; set; }
+        public bool IsDisliked { get; set; }
+        public bool IsWatchlisted { get; set; }
+    }
+}
