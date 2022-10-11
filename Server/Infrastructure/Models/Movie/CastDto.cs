f6dc0ab5b8cc5f4dc37191bcc7ba4df9dc853ded
Kiko <kristiqnenchevv@gmail.com>
2022-10-11 11:47:21 +0300

    Cast and Genere dto's create

---
+++ Server/Infrastructure/Models/Movie/CastDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Models.Movie
+{
+    public class CastDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Character { get; set; }
+        public string ProfilePath { get; set; }
+        public int Order { get; set; }
+        public string Department { get; set; }
+        public decimal Popularity { get; set; }
+    }
+}
---
+++ Server/Infrastructure/Models/Movie/GenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Models.Movie
+{
+    public class GenreDto
+    {
+        public int TmdbId { get; set; }
+        public string Name { get; set; }
+    }
+}
