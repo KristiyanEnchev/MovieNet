a8ebd36ef6e776332886520f2b0e1efb6cffa711
Kiko <kristiqnenchevv@gmail.com>
2022-10-12 14:37:30 +0300

    transformation or external api resul to my result

---
+++ Server/Infrastructure/Infrastructure/Services/Helpers/PaginationTransformer.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Services.Helpers
+{
+    using Models.Tmdb;
+
+    using Shared;
+
+    public static class PaginationTransformer
+    {
+        public static PaginatedResult<T> TransformToPaginatedResult<T>(TmdbPagedResponse<T> tmdbResponse)
+        {
+            return PaginatedResult<T>.Create(
+                data: tmdbResponse.Results,
+                count: tmdbResponse.TotalResults,
+                pageNumber: tmdbResponse.Page,
+                pageSize: tmdbResponse.Results?.Count ?? 0
+            );
+        }
+    }
+}
