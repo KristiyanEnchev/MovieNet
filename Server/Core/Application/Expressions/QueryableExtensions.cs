515faf16e808db3eabd1d7241c25a0aab69977a1
Kiko <kristiqnenchevv@gmail.com>
2022-10-14 14:03:32 +0300

    Create queriable extention

---
+++ Server/Core/Application/Expressions/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Application.Expressions
+{
+    using System.Linq.Expressions;
+
+    using Shared;
+
+    public static class QueryableExtensions
+    {
+        public static async Task<PaginatedResult<TResult>> ToPaginatedListAsync<TSource, TResult>(
+            this IQueryable<TSource> source,
+            int pageNumber,
+            int pageSize,
+            Func<TSource, TResult> mapper,
+            CancellationToken cancellationToken)
+            where TSource : class
+            where TResult : class
+        {
+            pageNumber = pageNumber == 0 ? 1 : pageNumber;
+            pageSize = pageSize == 0 ? 10 : pageSize;
+            int count = source.Count();
+            pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+            List<TSource> items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            List<TResult> mappedItems = items.Select(mapper).ToList();
+            return PaginatedResult<TResult>.Create(mappedItems, count, pageNumber, pageSize);
+        }
+
+        public static IQueryable<T> Sort<T, TKey>(this IQueryable<T> queryable, Expression<Func<T, TKey>> keySelector, bool ascending)
+        {
+            return ascending ? queryable.OrderBy(keySelector) : queryable.OrderByDescending(keySelector);
+        }
+    }
+}
