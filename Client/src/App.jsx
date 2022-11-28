07d54e4bb402750ae80052e7ee102b8a7eaa787a
Kiko <kristiqnenchevv@gmail.com>
2022-11-28 11:43:58 +0200

    Create Navbar and add it to the pipeline

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ErrorBoundary from './components/shared/ErrorBoundary';
+import { Navbar } from './components/Navbar';
 
 function App() {
-  return <ErrorBoundary></ErrorBoundary>;
+  return (
+    <ErrorBoundary>
+      <div className="min-h-screen flex flex-col bg-background text-foreground">
+        <Navbar />
+        <main className="flex-1 pt-18"> </main>
+      </div>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Film, Home, Tv } from 'lucide-react';
+
+const navItems = [
+  { path: '/', icon: Home, label: 'Home' },
+  { path: '/movies', icon: Film, label: 'Movies' },
+  { path: '/shows', icon: Tv, label: 'Shows' },
+];
+
+export function Navbar() {
+  const location = useLocation();
+
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/20 backdrop-blur-md border-b border-border/40">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <Link
+            to="/"
+            className="text-xl font-bold text-primary hover:opacity-80 transition-opacity"
+          >
+            MovieNet
+          </Link>
+
+          <div className="flex items-center space-x-4">
+            {navItems.map(({ path, icon: Icon, label }) => {
+              const isActive = location.pathname === path;
+              return (
+                <Link
+                  key={path}
+                  to={path}
+                  className={`flex items-center px-3 py-2 rounded-md transition-all ${
+                    isActive
+                      ? 'text-primary bg-accent/30'
+                      : 'text-muted-foreground hover:text-primary hover:bg-accent/20'
+                  }`}
+                >
+                  <Icon className="h-5 w-5" />
+                  <span className="ml-2 hidden sm:inline">{label}</span>
+                </Link>
+              );
+            })}
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <Link
+              to="/login"
+              className="text-muted-foreground hover:text-primary transition-colors"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
