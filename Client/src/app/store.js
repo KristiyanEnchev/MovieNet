cf4b0c8f6aaf298e1415a3bbe505763ea870939e
Kiko <kristiqnenchevv@gmail.com>
2022-12-13 11:19:42 +0200

    Update the store to acomodate the media slice

--- Client/src/app/store.js
+++ Client/src/app/store.js
@@ -1,8 +1,16 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+import { mediaApi } from '../features/media/mediaApi';
 import { authApi } from '../features/auth/authApi';
 import authReducer from '../features/auth/authSlice';
 
+const logger = createLogger({
+  collapsed: true,
+  diff: true,
+});
+
 const combinedReducer = combineReducers({
+  [mediaApi.reducerPath]: mediaApi.reducer,
   [authApi.reducerPath]: authApi.reducer,
   auth: authReducer,
 });
@@ -10,7 +18,7 @@ const combinedReducer = combineReducers({
 const rootReducer = (state, action) => {
   if (action.type === 'auth/clearCredentials') {
     state = undefined;
-    authApi.util.resetApiState();
+    mediaApi.util.resetApiState();
   }
   return combinedReducer(state, action);
 };
@@ -18,6 +26,12 @@ const rootReducer = (state, action) => {
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
-  devTools: true,
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    })
+      .concat(mediaApi.middleware)
+      .concat(authApi.middleware)
+      .concat(process.env.NODE_ENV !== 'production' ? logger : []),
+  devTools: process.env.NODE_ENV !== 'production',
 });
