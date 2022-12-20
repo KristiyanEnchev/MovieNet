89a491430b6fd78b3121844a64f80344317b004c
Kiko <kristiqnenchevv@gmail.com>
2022-12-20 12:36:33 +0200

    Integrate the search context needed for the searchbar implementation

---
+++ Client/src/contexts/SearchContext.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const SearchContext = createContext();
+
+export function SearchProvider({ children }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+
+  const value = {
+    searchQuery,
+    setSearchQuery,
+    searchResults,
+    setSearchResults,
+    isSearching,
+    setIsSearching,
+  };
+
+  return (
+    <SearchContext.Provider value={value}>{children}</SearchContext.Provider>
+  );
+}
+
+export function useSearch() {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error('useSearch must be used within a SearchProvider');
+  }
+  return context;
+}
